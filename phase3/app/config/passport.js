50623de8c21a7bf5b9e07afcb659e20f829b6bd9
wei jun zeng <weijun.zeng@mail.utoronto.ca>
2016-07-23 22:30:51 -0400

    update routes.js & passport add admin-login

--- phase3/app/config/passport.js
+++ phase3/app/config/passport.js
@@ -173,6 +173,44 @@ module.exports = function(passport) {
     
     
     
+    // =========================================================================
+    // ADMIN LOGIN =============================================================
+    // =========================================================================
+
+    passport.use('admin-login', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, email, password, done) { // callback with email and password from our for
+        // admin uses admin@bemaster.com email only
+        if (email != 'admin@bemaster.com') {
+            return done(null, false, req.flash('loginMessage', 'Sorry, but you are not an administrater.'));
+        } else {
+            // find a user whose email is admin@bemaster.com
+            User.findOne({ 'local.email' :  'admin@bemaster.com' }, function(err, user) {
+                // if there are any errors, return the error before anything else
+                if (err)
+                    return done(err);
+
+                // if no user is found, return the message
+                if (!user)
+                    return done(null, false, req.flash('loginMessage', 'No admin found.')); // req.flash is the way to set flashdata using connect-flash
+    
+                // if the user is found but the password is wrong
+                if (!user.validPassword(password))
+                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+    
+                // all is well, return successful user
+                return done(null, user);
+            });
+        }
+
+    }));
+    
+    
+    
     
     
     
