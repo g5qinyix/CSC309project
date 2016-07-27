612826c974686812722cb8974d532e0925fec721
YIXIONG CHEN <jack.chenyixiong@gmail.com>
2016-07-26 22:00:10 -0400

    unit test done

---
+++ phase3/app/test/README.txt
@@ -0,0 +1,4 @@
+Command to run test:
+
+        1. npm install (install all modules needed at root)
+        2. npm test (in test dir)
--- phase3/app/test/test.js
+++ phase3/app/test/test.js
@@ -3,6 +3,8 @@ var User, app, mongoose, request, server, should, agent;
 should   = require("should");
 app      = require("../server");
 User     = require("../app/models/user");
+Message  = require("../app/models/message");
+Comment  = require("../app/models/comment");
 request  = require("supertest");
 agent = request.agent(app)
 var path     = require('path');
@@ -57,9 +59,10 @@ describe('Student', function () {
           var oldPath = path.join(__dirname, '../public', user.local.photo);
           fs.unlinkSync(oldPath);
           user.remove();
+           return done();
      
         });
-      return done();
+     
     });
 })
 
@@ -94,10 +97,11 @@ describe('Coach', function () {
          var oldPath = path.join(__dirname, '../public', user.local.photo);
           fs.unlinkSync(oldPath);
           user.remove();
+            return done();
       });
  
   
-      return done();
+    
     });
 })
 
@@ -134,8 +138,9 @@ describe('Coach', function () {
             var oldPath = path.join(__dirname, '../public', user.local.photo);
           fs.unlinkSync(oldPath);
           user.remove();
+           return done();
         });
-      return done();
+     
     });
 })
 
@@ -167,8 +172,11 @@ describe('User', function () {
   });
       
   after(function(done) {
-      User.remove({"local.email" : 'test@mail.com'}).exec();
-      return done();
+      User.remove({"local.email" : 'test@mail.com'}, function(){
+          return done();
+        })
+       
+                                                        
     });
 });
   
@@ -213,6 +221,7 @@ describe('Search', function(){
 });
   
 
+
 // =====================================
 // PROFILE SECTION =====================
 // =====================================
@@ -228,7 +237,7 @@ describe('UserProfile', function () {
 	
     });
   
-  describe('Profle test', function () {
+  describe('Profle Security test', function () {
       it('should redirect to /', function (done) {
         agent
         .get('/profile')
@@ -239,27 +248,31 @@ describe('UserProfile', function () {
   });
       
   after(function(done) {
-      User.remove({"local.email" : 'userprofiletest@mail.com'}).exec();
-      return done();
+      User.remove({"local.email" : 'userprofiletest@mail.com'}, function(){
+         return done();
+        
+        });
+
     });
 });
 
 
+//User is login the can access his profile
 describe('UserProfile', function () { 
   before(function(done) {
       var user  = new User();
-      user.local.email ="testviewcoachprofile@mail.com";
+      user.local.email ="userprofiletest2@mail.com";
       user.local.password = user.generateHash("123456");
       user.save();
       agent
         .post('/login')
-        .field('email', 'testviewcoachprofile@mail.com')
+        .field('email', 'userprofiletest2@mail.com')
         .field('password', '123456')
         .expect('Location','/home')
         .end(done)   
     });
   
-  describe('Login test', function () {
+  describe('Login in - Profile ', function () {
       it('should redirect to /profile', function (done) {
         agent
         .get('/profile')
@@ -267,12 +280,182 @@ describe('UserProfile', function () {
         .end(done)
       })
   });
-      
+  
   after(function(done) {
-      User.remove({"local.email" : 'testviewcoachprofiletest@mail.com'}).exec();
-      return done();
+      User.remove({"local.email" : 'userprofiletest2@mail.com'}, function(err){
+         return done();
+        });
+     
     });
 });
-  
 
+
+
+
+// =====================================
+// View coach ==========================
+// =====================================
+describe('View coach infomation', function () { 
+  before(function(done) {
+      //create as student and login in
+      var user  = new User();
+      user.local.email ="testviewcoachprofile@mail.com";
+      user.local.password = user.generateHash("123456");
+      user.save();
+      //create a coach
+      var coach = new User();
+      coach.local.email = "testviewcoachprofile-coach@mail.com"
+      coach.local.password = coach.generateHash("123456");
+      coach.local.occupation = "coach"
+      coach.local.game = "CS:GO"
+      coach.local.coachtype = "Online"
+      coach.save();
+      agent
+        .post('/login')
+        .field('email', 'testviewcoachprofile@mail.com')
+        .field('password', '123456')
+        .expect('Location', '/home')
+        .end(done)   
+    });
+  
+  
+  describe('View coach', function () {
+      it('should link to /users/user_id', function (done) {
+        User.findOne({"local.email": "testviewcoachprofile-coach@mail.com"}).exec(function(err, coach){
+         var url ='/users/' + coach._id
+         agent
+        .get(url)
+        .expect(200)
+        .end(done)  
+          });
+    
+      })
+  });
+  
+  after(function(done) {
+      User.remove({"local.email" : 'testviewcoachprofile@mail.com'},function(err){
+          User.remove({"local.email": "testviewcoachprofile-coach@mail.com"}).exec();
+           return done();
+            
+        });
+
+     
+    });
+});
+
+
+
+
+// =====================================
+// Send Message ========================
+// =====================================
+describe('Send Message', function () {
+  
+  before(function(done) {
+      //create as student and login in
+      var user  = new User();
+      var id = 
+      user.local.email ="testsendmessage@mail.com";
+      user.local.password = user.generateHash("123456");
+      user.save();
+      //create a coach
+      var coach = new User();
+      coach.local.email = "testsendmessage-coach@mail.com"
+      coach.local.password = coach.generateHash("123456");
+      coach.local.occupation = "coach"
+      coach.local.game = "CS:GO"
+      coach.local.coachtype = "Online"
+      coach.save();
+      agent
+        .post('/login')
+        .field('email', 'testsendmessage@mail.com')
+        .field('password', '123456')
+        .expect('Location', '/home')
+        .end(done)
+        
+    });
+  
+  describe('Send hello to a coach', function () {
+      it('send request /messages/user_id', function (done) {
+        User.findOne({"local.email": "testsendmessage-coach@mail.com"}).exec(function(err, coach){
+         var url ='/message/' + coach._id
+         agent
+        .post(url)
+        .field('content',"hello")
+        .expect(302)
+        .end(done)  
+          });
+    
+      })
+  });
+  
+  after(function(done) {
+      User.remove({"local.email" : 'testsendmessage@mail.com'}).exec(function(){
+              User.remove({"local.email": "testsendmessage-coach@mail.com"}, function(err){
+                Message.remove().exec(function(err){
+                   return done();
+                })
+                });  
+        });
+    });
+});
+
+
+
+
+// =====================================
+// Comment and rate ====================
+// =====================================
+describe('Comment and Rate', function () {
+  
+  before(function(done) {
+      //create as student and login in
+      var user  = new User();
+      var id = 
+      user.local.email ="testcomment@mail.com";
+      user.local.password = user.generateHash("123456");
+      user.save();
+      //create a coach
+      var coach = new User();
+      coach.local.email = "testcomment-coach@mail.com"
+      coach.local.password = coach.generateHash("123456");
+      coach.local.occupation = "coach"
+      coach.local.game = "CS:GO"
+      coach.local.coachtype = "Online"
+      coach.save();
+      agent
+        .post('/login')
+        .field('email', 'testcomment@mail.com')
+        .field('password', '123456')
+        .expect('Location', '/home')
+        .end(done)
+        
+    });
+  
+  describe('Send a comment to a coach', function () {
+      it('send request /comments/user_id', function (done) {
+        User.findOne({"local.email": "testcomment-coach@mail.com"}).exec(function(err, coach){
+         var url ='/comments/' + coach._id
+         agent
+        .post(url)
+        .field('comment',"good coach")
+        .field('rate',  10)
+        .expect(302)
+        .end(done)  
+         });
+    
+      })
+  });
+  
+  after(function(done) {
+      User.remove({"local.email" : 'testcomment@mail.com'}).exec(function(){
+              User.remove({"local.email": "testcomment-coach@mail.com"}, function(err){
+               Comment.remove().exec(function(err){
+                  return done();
+               })
+              });  
+        });
+     
+    });
+});
 
